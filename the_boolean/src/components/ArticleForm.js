cbdece2f8fe2bf9c96c25b58734558b38a453ffe
Anthony Starke <starke@gmail.com>
2019-04-24 14:43:25 +0100

    Can add article with all suitable fields

--- the_boolean/src/components/ArticleForm.js
+++ the_boolean/src/components/ArticleForm.js
@@ -10,7 +10,8 @@ function handleSubmit(evt) {
     "text": evt.target.text.value,
     "datePublished": evt.target.datePublished.value,
     "image": "",
-    "journalist": evt.target.journalist.value
+    "journalist": evt.target.journalist.value,
+    "category": evt.target.category.value
   }
   props.createArticle(article);
 }
@@ -21,12 +22,29 @@ const journalistOptions = props.newArticle.data.journalists.map((journalist, ind
   return (
     <div>
     <form onSubmit = {handleSubmit} >
+    Title:
+    <br/>
     <input type = "text" placeholder = "Add a title" name = "title" />
+    <br/>
+    Text:
     <br/>
     <input type = "text" placeholder = "Add some text" name = "text" />
     <br/>
+    Date:
+    <br/>
     <input type = "text" placeholder = "Add a date" name = "datePublished" />
     <br/>
+    <p> Add a Category </p>
+    <select name = "category">
+    <option value = "health" > Health </option>
+    <option value = "community" > Community </option>
+    <option value = "controversy" > Controversy </option>
+    <option value = "education" > Education </option>
+    <option value = "career-news" > Career News </option>
+
+    </select>
+    <br/>
+    <p> Select a Journalist </p>
     <select name = "journalist">
     {journalistOptions}
     </select>
